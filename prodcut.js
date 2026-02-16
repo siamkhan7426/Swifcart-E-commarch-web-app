9adac66fe25c3ffc5ece661a9db5b4053af8f326
Siam Ahmed <sieam.khan12@gmail.com>
2026-02-16 22:24:40 +0600

    prodcut dynamic modal show

--- prodcut.js
+++ prodcut.js
@@ -1,3 +1,28 @@
+// ------------------------- loadidFunction dynamic api star------------------------
+const loadIdFucntion = async (id)=>{
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`)
+  const data = await res.json();
+  loadDynamicModalFunction(data);
+}
+// ------------------------- loadidFunction dynamic api end------------------------
+// -----------------------------dynamic modal show star--------------------
+const loadDynamicModalFunction = (modal)=>{
+  const { description,  rating:rate, rating: count, title }= modal
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = `
+          <div class="space-y-2">
+         <h1 class="text-2xl font-semibold">${title}</h1>
+        <p class="text-sm">${description}</p>
+        <span ><i class="fa-solid fa-star text-amber-400 font-semibold"></i> ${rate.rate} (${count.count})</span>
+        <div class="mt-5" class="flex gap-4 md:gap-6 items-center">
+        <button class="btn btn-primary">By Now</button>
+        <button class="btn btn-primary">Add Cart</button>
+      </div>
+    </div>
+  `
+document.getElementById("my_modal").showModal()
+};
+// -----------------------------dynamic modal show end--------------------
 
 
 // -------------------------trendingDataLoadAPi function create----------------
@@ -12,7 +37,7 @@ const displayCardTrending = (Cards) => {
     console.log(Cards);
     const AllProdcutCardContainer = document.getElementById("all-procut-card-container");
     Cards.forEach(cardElement => { 
-        const { category, description, image, price, rating:rate, rating: count, title } = cardElement
+        const { category,id, description, image, price, rating:rate, rating: count, title } = cardElement
         const div = document.createElement("div");
         div.innerHTML = `
        <div class="w-full h-full border-1 border-gray-300 rounded-lg space-y-2">
@@ -26,7 +51,7 @@ const displayCardTrending = (Cards) => {
     <h3 class="text-lg font-semibold text-justify px-4">${description.slice(0,40)}</h3>
     <h2 class="text-lg font-semibold px-4">$${price}</h2>
     <div class="flex  justify-between px-4 items-center mb-4 ">
-      <button onclick="showModal() class="btn btn-soft"><i class="fa-regular fa-eye"></i>Details</button>
+      <button onclick="loadIdFucntion(${id})"  class="btn btn-outline"><i class="fa-regular fa-eye"></i>Details</button>
       <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i> Add</button>
     </div>
   </div>
